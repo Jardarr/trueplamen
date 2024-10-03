bfdae9bff8504a07eba8daf0ff7e518f40116ecd
Jardarr <dmitrynordmann@gmail.com>
2024-10-03 17:54:49 +0300

    fix: animation

--- app/components/animate.tsx
+++ app/components/animate.tsx
@@ -5,9 +5,9 @@ import gsap from "gsap";
 export default function AnimatedContent() {
 	useEffect(() => {
 		gsap.to(".wall", {
-			delay: 5,
+			delay: 0.2,
 			opacity: 0,
-			duration: 3,
+			duration: 0.5,
 			ease: "power1.inOut",
 			onComplete: () => {
 				const wallElement = document.querySelector(
@@ -20,27 +20,10 @@ export default function AnimatedContent() {
 		});
 
 		gsap.to(".heroCard", {
-			delay: 6,
+			delay: 0.4,
 			opacity: 1,
-			duration: 3,
+			duration: 0.5,
 			ease: "power1.inOut",
-		});
-
-		gsap.to(".stanza", {
-			delay: 0.5,
-			opacity: 1,
-			duration: 1.5,
-			ease: "power1.inOut",
-		});
-
-		gsap.to("body", {
-			duration: 8,
-			onStart: () => {
-				document.body.style.overflowY = "hidden";
-			},
-			onComplete: () => {
-				document.body.style.overflowY = "";
-			},
 		});
 	}, []);
 
--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
         <div className="h-custom-height flex justify-center items-center bg-hero-bg bg-cover bg-center bg-attachment-fixed bg-no-repeat">
             <AnimatedContent />
             <div className="wall flex flex-col items-center justify-center opacity-100 absolute w-full h-full top-0 bg-black text-white text-xs font-thin">
-                <p className="stanza opacity-0 px-5">
+                {/* <p className="stanza opacity-0 px-5">
                     <i>
                         Вы пришли в мир, чтоб гореть в солнце Града…
                         <br />А чем лютей зло, тем ярче пламень чистых сердец!
@@ -14,7 +14,7 @@ export default function Hero() {
                 </p>
                 <p className="stanza opacity-0 max-w-[340px] w-full text-right px-5 md:px-0">
                     &quot;Пламень&quot; Пимен Карпов
-                </p>
+                </p> */}
             </div>
             <div className="heroCard opacity-0">
                 <HeroLogo />
