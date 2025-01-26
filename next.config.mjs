1ff27e7da4584ed52e4b4fcc823905e47682840a
Jardarr <dmitrynordmann@gmail.com>
2025-01-27 00:35:04 +0300

    fix: add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
@@ -13,6 +13,27 @@ const nextConfig = {
 				pathname: "/**",
 			},
 		],
+	},
+	async redirects() {
+		return [
+			{
+				source: "/news/king-ov-life-and-death", // Старый путь
+				has: [
+					{
+						type: "header",
+						key: "accept-language", // Читаем заголовок Accept-Language
+						value: "(ru).*", // Проверяем, содержит ли язык "ru"
+					},
+				],
+				destination: "/ru/news/king-ov-life-and-death", // Редирект на русскую страницу
+				permanent: false, // Временный редирект (307)
+			},
+			{
+				source: "/news/king-ov-life-and-death",
+				destination: "/en/news/king-ov-life-and-death", // Редирект по умолчанию (английский)
+				permanent: false, // Временный редирект (307)
+			},
+		];
 	},
 };
 
