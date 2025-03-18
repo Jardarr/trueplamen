3a8fbe57d14efa65fb7c07e2f46f0a641285d95b
Jardarr <dmitrynordmann@gmail.com>
2025-03-18 23:31:35 +0300

    feat: add badge shadcn ui

--- src/app/components/newsPreview/prewiewVoxOuroborosDagaz.tsx
+++ src/app/components/newsPreview/prewiewVoxOuroborosDagaz.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import DOMPurify from "isomorphic-dompurify";
 import { useTranslations } from "next-intl";
 import { Link } from "../../../i18n/routing";
+import { Badge } from "@/components/ui/badge";
 
 export default function PreviewVoxOuroborosDagaz() {
 	const t = useTranslations("DagazPrewiew");
@@ -15,7 +16,8 @@ export default function PreviewVoxOuroborosDagaz() {
 						<Image className="h-full w-full object-cover rounded-t-md sm:rounded-l-md sm:rounded-r-none" src="/covers/vox-ouroboros-dagaz-BIG.jpg" alt="" width={150} height={150} />
 					</div>
 					<div className="flex-1 m-4">
-						<p className="text-gray-300 text-sm sm:text-xl">NEW !!!</p>
+						{/* <p className="text-gray-300 text-sm sm:text-xl">NEW !!!</p> */}
+						<Badge variant="destructive">NEW!!!</Badge>
 						<h1 className="text-gray-300 text-sm sm:text-base" dangerouslySetInnerHTML={{ __html: cleanTitle }}></h1>
 						<span className="text-gray-400 text-sm">20.03.2025</span>
 						<p className="news-link text-sm text-right flex items-center justify-end">
