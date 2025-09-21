034e52376aae84e9e00f49f4b9b2e7d9a5b29e12
Jardarr <dmitrynordmann@gmail.com>
2025-09-21 15:17:51 +0300

    feat: add fehu album news

--- src/app/components/newsPreview/previewKingOvLifeAndDeath.tsx
+++ src/app/components/newsPreview/previewKingOvLifeAndDeath.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import DOMPurify from "isomorphic-dompurify";
 import { useTranslations } from "next-intl";
 import { Link } from "../../../i18n/routing";
-import { Badge } from "@/components/ui/badge";
 
 export default function PreviewKingOvLifeAndDeath() {
     const t = useTranslations("KingPreview");
@@ -16,8 +15,6 @@ export default function PreviewKingOvLifeAndDeath() {
                         <Image className="h-full w-full object-cover rounded-t-md sm:rounded-l-md sm:rounded-r-none" src="/covers/king-ov-life-and-death-BIG-r.jpg" alt="" width={150} height={150} />
                     </div>
                     <div className="flex-1 m-4">
-						{/* <p className="text-gray-300 text-sm sm:text-xl">NEW !!!</p> */}
-                        <Badge variant="destructive">NEW!!!</Badge>
                         <h1 className="text-gray-300 text-sm sm:text-base" dangerouslySetInnerHTML={{ __html: cleanTitle }}></h1>
 						<span className="text-gray-400 text-sm">21.06.2025</span>
 						<p className="news-link text-sm text-right flex items-center justify-end">{t("more")}<ChevronRight size={16} /></p>
