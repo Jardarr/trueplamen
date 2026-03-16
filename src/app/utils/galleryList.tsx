e9ed68c120384abf3ab591113cdb79af4c6a021b
Jardarr <dmitrynordmann@gmail.com>
2026-03-16 22:17:16 +0300

    Feat: Transferred a gallery from a third-party service to the project

--- src/app/utils/galleryList.tsx
+++ src/app/utils/galleryList.tsx
@@ -4,42 +4,42 @@ const galleryList = [
             {
                 id: 1,
                 alt: "seeds_ov_hell-1",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/seeds-ov-hell/soh1.jpg",
+                src: "/gallery/seeds-ov-hell/soh1.jpg",
             },
             {
                 id: 2,
                 alt: "seeds_ov_hell-2",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/seeds-ov-hell/soh2.jpg",
+                src: "/gallery/seeds-ov-hell/soh2.jpg",
             },
             {
                 id: 3,
                 alt: "seeds_ov_hell-3",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/seeds-ov-hell/soh3.jpg",
+                src: "/gallery/seeds-ov-hell/soh3.jpg",
             },
             {
                 id: 4,
                 alt: "seeds_ov_hell-4",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/seeds-ov-hell/soh4.jpg",
+                src: "/gallery/seeds-ov-hell/soh4.jpg",
             },
             {
                 id: 5,
                 alt: "seeds_ov_hell-5",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/seeds-ov-hell/soh5.jpg",
+                src: "/gallery/seeds-ov-hell/soh5.jpg",
             },
             {
                 id: 6,
                 alt: "seeds_ov_hell-6",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/seeds-ov-hell/soh6.jpg",
+                src: "/gallery/seeds-ov-hell/soh6.jpg",
             },
             {
                 id: 7,
                 alt: "seeds_ov_hell-7",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/seeds-ov-hell/soh7.jpg",
+                src: "/gallery/seeds-ov-hell/soh7.jpg",
             },
             {
                 id: 8,
                 alt: "seeds_ov_hell-8",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/seeds-ov-hell/soh8.jpg",
+                src: "/gallery/seeds-ov-hell/soh8.jpg",
             },
         ]
     },
@@ -48,47 +48,47 @@ const galleryList = [
             {
                 id: 1,
                 alt: "for_the_sun-1",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/for-the-sun/fts1.jpg",
+                src: "/gallery/for-the-sun/fts1.jpg",
             },
             {
                 id: 2,
                 alt: "for_the_sun-2",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/for-the-sun/fts2.jpg",
+                src: "/gallery/for-the-sun/fts2.jpg",
             },
             {
                 id: 3,
                 alt: "for_the_sun-3",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/for-the-sun/fts3.jpg",
+                src: "/gallery/for-the-sun/fts3.jpg",
             },
             {
                 id: 4,
                 alt: "for_the_sun-4",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/for-the-sun/fts4.jpg",
+                src: "/gallery/for-the-sun/fts4.jpg",
             },
             {
                 id: 5,
                 alt: "for_the_sun-5",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/for-the-sun/fts5.jpg",
+                src: "/gallery/for-the-sun/fts5.jpg",
             },
             {
                 id: 6,
                 alt: "for_the_sun-6",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/for-the-sun/fts6.jpg",
+                src: "/gallery/for-the-sun/fts6.jpg",
             },
             {
                 id: 7,
                 alt: "for_the_sun-7",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/for-the-sun/fts7.jpg",
+                src: "/gallery/for-the-sun/fts7.jpg",
             },
             {
                 id: 8,
                 alt: "for_the_sun-8",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/for-the-sun/fts8.jpg",
+                src: "/gallery/for-the-sun/fts8.jpg",
             },
             {
                 id: 9,
                 alt: "for_the_sun-9",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/for-the-sun/fts9.jpg",
+                src: "/gallery/for-the-sun/fts9.jpg",
             },
         ]
     },
@@ -97,32 +97,32 @@ const galleryList = [
             {
                 id: 1,
                 alt: "to_ashes-1",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/to-ashes/ta1.jpg",
+                src: "/gallery/to-ashes/ta1.jpg",
             },
             {
                 id: 2,
                 alt: "to_ashes-2",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/to-ashes/ta2.jpg",
+                src: "/gallery/to-ashes/ta2.jpg",
             },
             {
                 id: 3,
                 alt: "to_ashes-3",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/to-ashes/ta3.jpg",
+                src: "/gallery/to-ashes/ta3.jpg",
             },
             {
                 id: 4,
                 alt: "to_ashes-4",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/to-ashes/ta4.jpg",
+                src: "/gallery/to-ashes/ta4.jpg",
             },
             {
                 id: 5,
                 alt: "to_ashes-5",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/to-ashes/ta5.jpg",
+                src: "/gallery/to-ashes/ta5.jpg",
             },
             {
                 id: 6,
                 alt: "to_ashes-6",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/to-ashes/ta6.jpg",
+                src: "/gallery/to-ashes/ta6.jpg",
             },
         ]
     },
@@ -131,42 +131,42 @@ const galleryList = [
             {
                 id: 1,
                 alt: "another_ones-1",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/another-ones/ao1.jpg",
+                src: "/gallery/another-ones/ao1.jpg",
             },
             {
                 id: 2,
                 alt: "another_ones-2",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/another-ones/ao2.jpg",
+                src: "/gallery/another-ones/ao2.jpg",
             },
             {
                 id: 3,
                 alt: "another_ones-3",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/another-ones/ao3.jpg",
+                src: "/gallery/another-ones/ao3.jpg",
             },
             {
                 id: 4,
                 alt: "another_ones-4",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/another-ones/ao4.jpg",
+                src: "/gallery/another-ones/ao4.jpg",
             },
             {
                 id: 5,
                 alt: "another_ones-5",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/another-ones/ao5.jpg",
+                src: "/gallery/another-ones/ao5.jpg",
             },
             {
                 id: 6,
                 alt: "another_ones-6",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/another-ones/ao6.jpg",
+                src: "/gallery/another-ones/ao6.jpg",
             },
             {
                 id: 7,
                 alt: "another_ones-7",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/another-ones/ao7.jpg",
+                src: "/gallery/another-ones/ao7.jpg",
             },
             {
                 id: 8,
                 alt: "another_ones-8",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/another-ones/ao8.jpg",
+                src: "/gallery/another-ones/ao8.jpg",
             },
         ]
     },
@@ -175,17 +175,17 @@ const galleryList = [
             {
                 id: 1,
                 alt: "anabasis_of_spirit-1",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/anabasis-of-spirit/ad1.jpg",
+                src: "/gallery/anabasis-of-spirit/ad1.jpg",
             },
             {
                 id: 2,
                 alt: "anabasis_of_spirit-2",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/anabasis-of-spirit/ad2.jpg",
+                src: "/gallery/anabasis-of-spirit/ad2.jpg",
             },
             {
                 id: 3,
                 alt: "anabasis_of_spirit-3",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/anabasis-of-spirit/ad3.jpg",
+                src: "/gallery/anabasis-of-spirit/ad3.jpg",
             },
         ]
     },
@@ -194,27 +194,27 @@ const galleryList = [
             {
                 id: 1,
                 alt: "magick_of_blood-1",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/magick-of-blood/mb1.jpg",
+                src: "/gallery/magick-of-blood/mb1.jpg",
             },
             {
                 id: 2,
                 alt: "magick_of_blood-2",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/magick-of-blood/mb2.jpg",
+                src: "/gallery/magick-of-blood/mb2.jpg",
             },
             {
                 id: 3,
                 alt: "magick_of_blood-3",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/magick-of-blood/mb3.jpg",
+                src: "/gallery/magick-of-blood/mb3.jpg",
             },
             {
                 id: 4,
                 alt: "magick_of_blood-4",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/magick-of-blood/mb4.jpg",
+                src: "/gallery/magick-of-blood/mb4.jpg",
             },
             {
                 id: 5,
                 alt: "magick_of_blood-5",
-                src: "https://fonqjzxztszpkmtoctbv.supabase.co/storage/v1/object/public/plamen_gallery/magick-of-blood/mb5.jpg",
+                src: "/gallery/magick-of-blood/mb5.jpg",
             },
         ]
     },
